b08e711345d4806c4b0a526505ef2d706591a8c9
Litchi Pi <litchi.pi@protonmail.com>
2022-08-09 11:11:32 +0200

    Improve library, add CI and build scripts
    Signed-off-by: Litchi Pi <litchi.pi@protonmail.com>

--- flake.nix
+++ flake.nix
@@ -37,45 +37,44 @@
         type = frontend.type;
       };
 
-      startRustyWebApp = {name, args, database, ...}@cfg: let
-        argsstr = builtins.concatStringsSep " " args;
-        start_database = let
-          pgctl_args = builtins.concatStringsSep " " [
-            "-D ${database.dir}"
-            "-l ${database.logfile}"
-            "-o \"-p ${builtins.toString database.port}\""
-            "-o \"--unix_socket_directories='${database.dir}'\""
-          ];
-        in pkgs.writeShellScript "${name}_start_database" ''
-          set -e
-          export PATH="$PATH:${pkgs.postgresql}/bin"
-          mkdir -p ${database.dir}
-          if [ ! -f ${database.dir}/PG_VERSION ]; then
-            initdb -D ${database.dir} --no-locale --encoding=UTF8
-            pg_ctl ${pgctl_args} start
-            createuser -p ${builtins.toString database.port} -h ${database.dir} -d ${database.user}
-            createdb -h ${database.dir} -p ${builtins.toString database.port} -U ${database.user} ${database.dbname}
-          else
-            pg_ctl ${pgctl_args} start
-          fi
-        '';
-  
-      # TODO  check connection to remote database
-      check_connection_db = pkgs.writeShellScript "check_connection_db" ''
-        # TODO
+      start_database = { name, database, ...}: let
+        pgctl_args = builtins.concatStringsSep " " [
+          "-D ${database.dir}"
+          "-l ${database.logfile}"
+          "-o \"-p ${builtins.toString database.port}\""
+          "-o \"--unix_socket_directories='${database.dir}'\""
+        ];
+      in pkgs.writeShellScript "${name}_start_database" ''
+        set -e
+        export PATH="$PATH:${pkgs.postgresql}/bin"
+        mkdir -p ${database.dir}
+        if [ ! -f ${database.dir}/PG_VERSION ]; then
+          initdb -D ${database.dir} --no-locale --encoding=UTF8
+          pg_ctl ${pgctl_args} start
+          createuser -p ${builtins.toString database.port} -h ${database.dir} -d ${database.user}
+          createdb -h ${database.dir} -p ${builtins.toString database.port} -U ${database.user} ${database.dbname}
+        else
+          pg_ctl ${pgctl_args} start
+        fi
       '';
 
-      dbstart = if database.local
-        then "${start_database}"
-        else "${check_connection_db}";
+      startRustyWebApp = {name, args, database, ...}@cfg: let
+        argsstr = builtins.concatStringsSep " " args;
+  
+        # TODO  check connection to remote database
+        check_connection_db = pkgs.writeShellScript "check_connection_db" ''
+        '';
+        dbstart = if database.local
+          then "${start_database cfg}"
+          else "${check_connection_db}";
 
-      dbstop = if database.local
-        then ''
-          if [ -f ${database.dir}/postmaster.pid ]; then
-            ${pkgs.postgresql}/bin/pg_ctl -D ${database.dir} stop
-          fi
-        ''
-        else "";
+        dbstop = if database.local
+          then ''
+            if [ -f ${database.dir}/postmaster.pid ]; then
+              ${pkgs.postgresql}/bin/pg_ctl -D ${database.dir} stop
+            fi
+          ''
+          else "";
       in pkgs.writeShellScript "${name}_start" ((if database.enable
         then ''
           function interrupt() {
@@ -97,7 +96,48 @@
         '' else ""
       ));
 
-      buildCi = ""; # TODO  Build CI to test and validate
+      buildCi = config: let
+        add_deps = builtins.concatStringsSep "\n" (builtins.map
+          (dep:
+            "export PATH=\"$PATH:${dep}/bin\""
+          ) (config.ci.deps pkgs)
+        );
+        prepare_ci = ''
+          ${add_deps}
+        '';
+      in pkgs.writeShellScript "${config.name}_ci" (''
+        ${prepare_ci}
+        # Executing CI scripts on backend
+        cd ${config.backend.src}
+        '' + (builtins.concatStringsSep "\n" (config.ci.backend_scripts)) + "\n" + ''
+        # Executing CI scripts on frontend
+        cd ${config.frontend.src}
+      '' + (builtins.concatStringsSep "\n" (config.ci.frontend_scripts))
+      );
+
+      buildFlake = config: {
+        packages = {
+          default = startRustyWebApp config;
+          dbstart = start_database config;
+          backend = build_backend config.backend;
+          frontend = build_frontend config.name config.frontend;
+          ci = buildCi config;
+        };
+
+        apps = {
+          default = {
+            type = "app";
+            program = "${startRustyWebApp config}";
+          };
+
+          ci = {
+            type = "app";
+            program = "${buildCi config}";
+          };
+        };
+
+        # TODO  Add NixOS module
+      };
     };
   });
 }
